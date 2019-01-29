a6e2492889ef2a8cc8546fc4ac248ff814deb539
justmobiledev <justmobiledev1@gmail.com>
2019-01-29 04:29:04 -0500

    list item activity updates.

--- app/src/main/java/com/justmobiledev/developerportfolio/ItemListActivity.kt
+++ app/src/main/java/com/justmobiledev/developerportfolio/ItemListActivity.kt
@@ -17,14 +17,7 @@ import kotlinx.android.synthetic.main.activity_item_list.*
 import kotlinx.android.synthetic.main.item_list_content.view.*
 import kotlinx.android.synthetic.main.item_list.*
 
-/**
- * An activity representing a list of Pings. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a [ItemDetailActivity] representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- */
+// Main Activity
 class ItemListActivity : AppCompatActivity() {
 
     companion object {
@@ -51,14 +44,9 @@ class ItemListActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.title = title
 
-        /* fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }*/
-
         if (item_detail_container != null) {
             // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
+            // large-screen layouts (res/layout-w900dp).
             // If this view is present, then the
             // activity should be in two-pane mode.
             twoPane = true
